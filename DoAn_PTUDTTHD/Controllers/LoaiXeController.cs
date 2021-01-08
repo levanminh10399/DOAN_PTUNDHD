7bd32f70ead7fe465e10e93fca6ab8fc8be8dfa3
Nguyen Quang Phuc <nguyenquangphuc.vn.hue@gmail.com>
2021-01-08 16:27:17 +0700

    loaixe

---
+++ DoAn_PTUDTTHD/Controllers/LoaiXeController.cs
@@ -0,0 +1,43 @@
+﻿using DoAn_PTUDTTHD.Models;
+using DoAn_PTUDTTHD.Repository;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace DoAn_PTUDTTHD.Controllers
+{
+    public class LoaiXeController : ApiController
+    {
+        LoaiXeRepository loaiXeRepository = new LoaiXeRepository();
+        // GET api/loaixe
+        public IEnumerable<LoaiXe> Get()
+        {
+            return loaiXeRepository.findAll();
+        }
+
+        // GET api/loaixe/2
+        public LoaiXe Get(int id)
+        {
+            return loaiXeRepository.findById(id);
+        }
+
+        // POST api/loaixe
+
+        public bool Post([FromBody] LoaiXe loaiXe)
+        {
+            return loaiXeRepository.addLoaiXe(loaiXe);
+        }
+
+        // PUT api/loaixe
+
+        public bool Put([FromBody] LoaiXe loaiXe)
+        {
+            return loaiXeRepository.updateLoaiXe(loaiXe);
+        }
+
+        // DELETE api/xe/1
+        public bool Delete(int id)
+        {
+            return loaiXeRepository.deleteXe(id);
+        }
+    }
+}
---
+++ DoAn_PTUDTTHD/Repository/LoaiXeRepository.cs
@@ -0,0 +1,90 @@
+﻿using DoAn_PTUDTTHD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DoAn_PTUDTTHD.Repository
+{
+    public class LoaiXeRepository
+    {
+        public List<LoaiXe> findAll()
+        {
+            using (var db = new QLHTGTEntities())
+            {
+                List<LoaiXe> loaiXes = db.LoaiXes.ToList();
+                if (loaiXes != null)
+                    return loaiXes;
+            }
+            return null;
+        }
+        public LoaiXe findById(int id)
+        {
+            using (var db = new QLHTGTEntities())
+            {
+                LoaiXe loaiXe = db.LoaiXes.Where(b => b.ID == id).FirstOrDefault();
+                if (loaiXe != null)
+                    return loaiXe;
+            }
+            return null;
+        }
+        public bool addLoaiXe(LoaiXe loaiXe)
+        {
+            using (var db = new QLHTGTEntities())
+            {
+                try
+                {
+                    db.LoaiXes.Add(loaiXe);
+                    db.SaveChanges();
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+
+
+            }
+        }
+        public bool updateLoaiXe(LoaiXe loaiXe)
+        {
+            using (var db = new QLHTGTEntities())
+            {
+                try
+                {
+                    LoaiXe loaiXeUpdate = db.LoaiXes.Find(loaiXe.ID);
+                    if (loaiXe == null)
+                        return false;
+                    loaiXeUpdate.NhanHieu = loaiXe.NhanHieu;
+                    loaiXeUpdate.MauXe = loaiXe.MauXe;
+                    loaiXeUpdate.NamSX = loaiXe.NamSX;
+                
+                    db.SaveChanges();
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+
+        }
+        public bool deleteXe(int id)
+        {
+            using (var db = new QLHTGTEntities())
+            {
+                try
+                {
+                    LoaiXe loaiXeDel = db.LoaiXes.Find(id);
+                    db.LoaiXes.Remove(loaiXeDel);
+                    db.SaveChanges();
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
